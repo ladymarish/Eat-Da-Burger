226d32302754514226452bf5f88fa375111293d1
Marina-Saladukha <marina@reunionmarketing.com>
2018-05-05 03:39:04 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Handlebars.
+//var exphbs = require("express-handlebars");
+
+//app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+//app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+//var routes = require("./controllers/burgers_controller.js");
+
+//app.use(routes);
+
+app.listen(PORT, function() {
+   console.log("App now listening at localhost:" + PORT);
+ });
